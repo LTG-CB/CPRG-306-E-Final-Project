3984640daa1bd9d6d8bb8b8574ad7a3d6d0ea08f
LTG-CB <curtis.borson@edu.sait.ca>
2024-11-29 14:06:43 -0700

    and again...
    and again...

--- pages/api/transactions/transactions.js
+++ pages/api/transactions/transactions.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import authOptions from './auth/[...nextauth]';
+import authOptions from '../auth/[...nextauth]';
 import { Pool } from 'pg';
 
 const pool = new Pool({
